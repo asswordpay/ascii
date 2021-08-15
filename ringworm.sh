c4027807ed0774b89e46460eb4e8e27e282349a5
asswordpay <75650924+asswordpay@users.noreply.github.com>
2021-08-15 16:03:08 -0400

    Update ringworm.sh

--- ringworm.sh
+++ ringworm.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo" L       k                                   d                     h    b            "
-echo" L    ii k k                                 d              ii     h    b            "
-echo" L       kk   eee      aa     ggg ooo ooo  ddd     nnn  eee    ggg hhh  bbb  ooo rrr "
-echo" L    ii k k  e e     a a     g g o o o o d  d     n  n e e ii g g h  h b  b o o r   "
-echo" LLLL ii k  k ee      aaa     ggg ooo ooo  ddd     n  n ee  ii ggg h  h bbb  ooo r   "
-echo"                               g                                g                   "
-echo"                             ggg                              ggg              "
+echo " L       k                                   d                     h    b            "
+echo " L    ii k k                                 d              ii     h    b            "
+echo " L       kk   eee      aa     ggg ooo ooo  ddd     nnn  eee    ggg hhh  bbb  ooo rrr "
+echo " L    ii k k  e e     a a     g g o o o o d  d     n  n e e ii g g h  h b  b o o r   "
+echo " LLLL ii k  k ee      aaa     ggg ooo ooo  ddd     n  n ee  ii ggg h  h bbb  ooo r   "
+echo "                               g                                g                   "
+echo "                             ggg                              ggg              "
 sleep 3
 clear
 echo "RRRR                                      EEEE      l                "
